7b0404cb8897414b6b8fccbf849dcc05e443e59c
amin <aminmohammadmazreali@gmail.com>
2021-06-30 14:20:37 +0430

    add rest api class for AssanPardakht Gateway

--- src/Drivers/Asanpardakht/Asanpardakht.php
+++ src/Drivers/Asanpardakht/Asanpardakht.php
@@ -2,6 +2,7 @@
 
 namespace Shetabit\Multipay\Drivers\Asanpardakht;
 
+use GuzzleHttp\Client;
 use Shetabit\Multipay\Abstracts\Driver;
 use Shetabit\Multipay\Exceptions\InvalidPaymentException;
 use Shetabit\Multipay\Exceptions\PurchaseFailedException;
@@ -13,12 +14,40 @@ use Shetabit\Multipay\Request;
 
 class Asanpardakht extends Driver
 {
+    const TokenURL = '/Token';
+    const TimeURL = '/Time';
+    const TranResultURL = '/TranResult';
+    const CardHashURL = '/CardHash';
+    const SettlementURL = '/Settlement';
+    const VerifyURL = '/Verify';
+    const CancelURL = '/Cancel';
+    const ReverseURL = '/Reverse';
+
     /**
      * Invoice
      *
      * @var Invoice
      */
     protected $invoice;
+
+    /**
+     * Response
+     *
+     * @var object
+     */
+    protected $response;
+
+    /**
+     * PayGateTransactionId
+     *
+     */
+    protected $payGateTransactionId;
+
+    /**
+     * Merchant Config ID
+     *
+     */
+    protected $merchantConfigID;
 
     /**
      * Driver settings
@@ -38,6 +67,7 @@ class Asanpardakht extends Driver
     {
         $this->invoice($invoice);
         $this->settings = (object)$settings;
+        $this->merchantConfigID = 35619;
     }
 
     /**
@@ -46,25 +76,16 @@ class Asanpardakht extends Driver
      * @return string
      *
      * @throws PurchaseFailedException
-     * @throws \SoapFault
      */
     public function purchase()
     {
-        $client = $this->createSoapClient($this->settings->apiPurchaseUrl);
+        $result = $this->token();
 
-        $params = $this->preparePurchaseData();
-        $result = $client->RequestOperation($params);
-        if (!$result) {
-            throw  new PurchaseFailedException('خطای فراخوانی متد درخواست تراکنش.');
+        if (!isset($result['status_code']) or $result['status_code'] != 200) {
+            $this->purchaseFailed($result['status_code']);
         }
 
-        $result = $result->RequestOperationResult;
-        if ($result[0] != '0') {
-            $message = "خطای شماره " . $result . " رخ داده است.";
-            throw  new PurchaseFailedException($message);
-        }
-
-        $this->invoice->transactionId(substr($result, 2));
+        $this->invoice->transactionId($result['content']);
 
         // return the transaction's id
         return $this->invoice->getTransactionId();
@@ -77,10 +98,8 @@ class Asanpardakht extends Driver
      */
     public function pay(): RedirectionForm
     {
-        $payUrl = $this->settings->apiPaymentUrl;
-
         $data = [
-            'RefId' => $this->invoice->getTransactionId()
+            'RefID' => $this->invoice->getTransactionId()
         ];
 
         //set mobileap for get user cards
@@ -88,7 +107,7 @@ class Asanpardakht extends Driver
             $data['mobileap'] = $this->invoice->getDetails()['mobile'];
         }
 
-        return $this->redirectWithForm($payUrl, $data, 'POST');
+        return $this->redirectWithForm($this->settings->apiPaymentUrl, $data, 'POST');
     }
 
     /**
@@ -96,95 +115,63 @@ class Asanpardakht extends Driver
      *
      * @return mixed|Receipt
      *
-     * @throws InvalidPaymentException
-     * @throws \SoapFault
+     * @throws PurchaseFailedException
      */
     public function verify(): ReceiptInterface
     {
-        $encryptedReturningParamsString = Request::input('ReturningParams');
-        $returningParamsString = $this->decrypt($encryptedReturningParamsString);
-        $returningParams = explode(",", $returningParamsString);
+        $result = $this->transactionResult();
 
-        /**
-         * other data:
-         *   $amount = $returningParams[0];
-         *   $saleOrderId = $returningParams[1];
-         *   $refId = $this->invoice->getTransactionId() ?? $returningParams[2];
-         *   $resMessage = $returningParams[4];
-         *   $rrn = $returningParams[6];
-         *   $lastFourDigitOfPAN = $returningParams[7];
-         **/
-
-        $resCode = $returningParams[3];
-        $payGateTranID = $returningParams[5];
-
-        if ($resCode != '0' && $resCode != '00') {
-            $message = "خطای شماره " . $resCode . " رخ داده و تراکنش ناموفق بوده است.";
-            throw new InvalidPaymentException($message);
+        if (!isset($result['status_code']) or $result['status_code'] != 200) {
+            $this->purchaseFailed($result['status_code']);
         }
 
-        $client = $this->createSoapClient($this->settings->apiVerificationUrl);
-        $params = $this->prepareVerificationData($payGateTranID);
+        $this->payGateTransactionId = $result['content']['payGateTranID'];
 
-        // step1: verify
-        $this->verifyStep($client, $params);
+        //step1: verify
+        $verify_result = $this->verifyTransaction();
+        if (!isset($verify_result['status_code']) or $verify_result['status_code'] != 200) {
+            $this->purchaseFailed($verify_result['status_code']);
+        }
 
-        // step2: settle
-        $this->settleStep($client, $params);
+        //step2: settlement
+        $this->settlement();
 
-        $receipt = $this->createReceipt($payGateTranID);
+        $receipt = $this->createReceipt($this->payGateTransactionId);
         $receipt->detail([
-            'traceNo' => $payGateTranID,
-            'referenceNo' => $returningParams[6],
-            'transactionId' => $returningParams[2],
-            'cardNo' => $returningParams[7],
+            'traceNo' => $this->payGateTransactionId,
+            'referenceNo' => $result['RRN'],
+            'transactionId' => $result['RefID'],
+            'cardNo' => $result['CardNumber'],
         ]);
 
         return $receipt;
     }
 
-    /**
-     * payment verification step
-     *
-     * @param $client
-     * @param $params
-     *
-     * @throws InvalidPaymentException
-     */
-    protected function verifyStep($client, $params)
+    protected function callApi($method, $url, $data = [])
     {
-        $result = $client->RequestVerification($params);
-        if (!$result) {
-            throw new InvalidPaymentException("خطای فراخوانی متد وريفای رخ داده است.");
-        }
-
-        $result = $result->RequestVerificationResult;
-        if ($result != '500') {
-            $message = "خطای شماره: " . $result . " در هنگام Verify";
-            throw  new InvalidPaymentException($message);
-        }
-    }
-
-    /**
-     * payment settlement step.
-     *
-     * @param $client
-     * @param $params
-     *
-     * @throws InvalidPaymentException
-     */
-    protected function settleStep($client, $params)
-    {
-        $result = $client->RequestReconciliation($params);
-        if (!$result) {
-            throw new InvalidPaymentException('خطای فراخوانی متد تسويه رخ داده است.');
-        }
-
-        $result = $result->RequestReconciliationResult;
-        if ($result != '600') {
-            $message = "خطای شماره: " . $result . " در هنگام Settlement";
-            throw new InvalidPaymentException($message);
-        }
+        \Log::debug('PaymentTest:BeforeCallApiSection',[
+            'url'=>$url,
+            'data'=>$data
+        ]);
+        $client = new Client();
+        $response = $client->request($method, $url, [
+            "json" => $data,
+            "headers" => [
+                'Content-Type' => 'application/json',
+                'usr' => $this->settings->username,
+                'pwd' => $this->settings->password
+            ],
+            "http_errors" => false,
+        ]);
+        \Log::debug('PaymentTest:AfterCallApiSection',[
+            'url'=>$url,
+            'status_code' => $response->getStatusCode(),
+            'content' => json_decode($response->getBody()->getContents(), true)
+        ]);
+        return [
+            'status_code' => $response->getStatusCode(),
+            'content' => json_decode($response->getBody()->getContents(), true)
+        ];
     }
 
     /**
@@ -201,135 +188,128 @@ class Asanpardakht extends Driver
         return $receipt;
     }
 
-    /**
-     * Prepare data for payment verification
-     *
-     * @param $payGateTranID
-     *
-     * @return array
-     *
-     * @throws \SoapFault
-     */
-    protected function prepareVerificationData($payGateTranID)
+    public function token()
     {
-        $credentials = array(
-            $this->settings->username,
-            $this->settings->password
-        );
+        return $this->callApi('POST', $this->settings->apiRestPaymentUrl . self::TokenURL, [
+            'serviceTypeId' => 1,
+            'merchantConfigurationId' => $this->merchantConfigID,
+            'localInvoiceId' => crc32($this->invoice->getUuid()),
+            'amountInRials' => $this->invoice->getAmount(),
+            'localDate' => $this->getTime()['content'],
+            'callbackURL' => $this->settings->callbackUrl . "?" . http_build_query(['invoice' => crc32($this->invoice->getUuid())]),
+            'paymentId' => "0",
+            'additionalData' => '',
+        ]);
+    }
 
-        $encryptedCredentials = $this->encrypt(implode(',', $credentials));
+    public function reverse()
+    {
+        return $this->callApi('POST', $this->settings->apiRestPaymentUrl . self::ReverseURL, [
+            'merchantConfigurationId' => $this->merchantConfigID,
+            'payGateTranId' => $this->invoice->getUuid()
+        ]);
+    }
 
-        return array(
-            'merchantConfigurationID' => $this->settings->merchantId,
-            'encryptedCredentials' => $encryptedCredentials,
-            'payGateTranID' => $payGateTranID
-        );
+    public function cancel()
+    {
+        return $this->callApi('POST', $this->settings->apiRestPaymentUrl . self::CancelURL, [
+            'merchantConfigurationId' => $this->merchantConfigID,
+            'payGateTranId' => $this->payGateTransactionId
+        ]);
+    }
+
+    public function verifyTransaction()
+    {
+        return $this->callApi('POST', $this->settings->apiRestPaymentUrl . self::VerifyURL, [
+            'merchantConfigurationId' => $this->merchantConfigID,
+            'payGateTranId' => $this->payGateTransactionId
+        ]);
+    }
+
+    public function settlement()
+    {
+        return $this->callApi('POST', $this->settings->apiRestPaymentUrl . self::SettlementURL, [
+            'merchantConfigurationId' => $this->merchantConfigID,
+            'payGateTranId' => $this->payGateTransactionId
+        ]);
+    }
+
+    public function cardHash()
+    {
+        return $this->callApi('GET', $this->settings->apiRestPaymentUrl . self::CardHashURL, [
+            'merchantConfigurationId' => $this->merchantConfigID,
+            'localInvoiceId' => $this->invoice->getUuid()
+        ]);
+    }
+
+    public function transactionResult()
+    {
+        return $this->callApi('POST', $this->settings->apiRestPaymentUrl . self::SettlementURL, [
+            'merchantConfigurationId' => $this->merchantConfigID,
+            'localInvoiceId' => $this->invoice->getUuid()
+        ]);
+    }
+
+    public function getTime()
+    {
+        return $this->callApi('GET', $this->settings->apiRestPaymentUrl . self::TimeURL);
     }
 
     /**
-     * Prepare data for purchasing invoice
+     * Trigger an exception
      *
-     * @return array
+     * @param $status
      *
-     * @throws \SoapFault
+     * @throws PurchaseFailedException
      */
-    protected function preparePurchaseData()
+    protected function purchaseFailed($status)
     {
-        if (!empty($this->invoice->getDetails()['description'])) {
-            $description = $this->invoice->getDetails()['description'];
+        $translations = [
+            400 => "bad request",
+            401 => "unauthorized. probably wrong or unsent header(s)",
+            471 => "identity not trusted to proceed",
+            472 => "no records found",
+            473 => "invalid merchant username or password",
+            474 => "invalid incoming request machine ip. check response body to see your actual public IP address",
+            475 => "invoice identifier is not a number",
+            476 => "request amount is not a number",
+            477 => "request local date length is invalid",
+            478 => "request local date is not in valid format",
+            479 => "invalid service type id",
+            480 => "invalid payer id",
+            481 => "incorrect settlement description format",
+            482 => "settlement slices does not match total amount",
+            483 => "unregistered iban",
+            484 => "internal error for other reasons",
+            485 => "invalid local date",
+            486 => "amount not in range",
+            487 => "service not found or not available for merchant",
+            488 => "invalid default callback",
+            489 => "duplicate local invoice id",
+            490 => "merchant disabled or misconfigured",
+            491 => "too many settlement destinations",
+            492 => "unprocessable request",
+            493 => "error processing special request for other reasons like business restrictions",
+            494 => "invalid payment_id for governmental payment",
+            495 => "invalid referenceId in additionalData",
+            496 => "invalid json in additionalData",
+            497 => "invalid payment_id location",
+            571 => "misconfiguration OR not yet processed",
+            572 => "misconfiguration OR transaction status undetermined",
+            573 => "misconfiguraed valid ips for configuration OR unable to request for verification due to an internal error",
+            574 => "internal error in uthorization",
+            575 => "no valid ibans found for merchant",
+            576 => "internal error",
+            577 => "internal error",
+            578 => "no default sharing is defined for merchant",
+            579 => "cant submit ibans with default sharing endpoint",
+            580 => "error processing special request"
+        ];
+
+        if (array_key_exists($status, $translations)) {
+            throw new PurchaseFailedException($translations[$status]);
         } else {
-            $description = $this->settings->description;
+            throw new PurchaseFailedException('خطای ناشناخته ای رخ داده است.');
         }
-
-        // configs
-        $username = $this->settings->username;
-        $password = $this->settings->password;
-        $callBackUrl = $this->settings->callbackUrl;
-
-        // invoice details
-        $price = $this->invoice->getAmount() * 10; // convert to rial
-        $additionalData = $description ?? '';
-        $orderId = crc32($this->invoice->getUuid());
-        $localDate = date("Ymd His");
-
-        // box and encrypt everything
-        $requestString = "1,{$username},{$password},{$orderId},{$price},{$localDate},{$additionalData},{$callBackUrl},0";
-        $encryptedRequestString = $this->encrypt($requestString);
-
-        return array(
-            'merchantConfigurationID' => $this->settings->merchantId,
-            'encryptedRequest' => $encryptedRequestString
-        );
-    }
-
-    /**
-     * Encrypt given string.
-     *
-     * @param $string
-     *
-     * @return mixed
-     *
-     * @throws \SoapFault
-     */
-    protected function encrypt($string)
-    {
-        $client = $this->createSoapClient($this->settings->apiUtilsUrl);
-
-        $params = array(
-            'aesKey' => $this->settings->key,
-            'aesVector' => $this->settings->iv,
-            'toBeEncrypted' => $string
-        );
-
-        $result = $client->EncryptInAES($params);
-
-        return $result->EncryptInAESResult;
-    }
-
-    /**
-     * Decrypt given string.
-     *
-     * @param $string
-     *
-     * @return mixed
-     *
-     * @throws \SoapFault
-     */
-    protected function decrypt($string)
-    {
-        $client = $this->createSoapClient($this->settings->apiUtilsUrl);
-
-        $params = array(
-            'aesKey' => $this->settings->key,
-            'aesVector' => $this->settings->iv,
-            'toBeDecrypted' => $string
-        );
-
-        $result = $client->DecryptInAES($params);
-
-        return $result->DecryptInAESResult;
-    }
-
-    /**
-     * create a new SoapClient
-     *
-     * @param $url
-     *
-     * @return \SoapClient
-     *
-     * @throws \SoapFault
-     */
-    protected function createSoapClient($url)
-    {
-        $opts = array(
-            'ssl' => array(
-                'verify_peer' => false,
-                'verify_peer_name' => false
-            )
-        );
-
-        $configs = array('stream_context' => stream_context_create($opts));
-
-        return new \SoapClient($url, $configs);
     }
 }
