4f11bd52bcd57ff6519c12a2ca85b60f336357a7
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-14 17:44:10 +0430

    Add Payment unit test

---
+++ tests/PaymentTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Shetabit\Multipay\Tests;
+
+use Shetabit\Multipay\Payment;
+
+class PaymentTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertEquals($this->payment, $this->payment);
+    }
+}
