4d4957f87f93912e424dffc55c96b0871d4e81b1
Sapren Company <51704286+saprenco@users.noreply.github.com>
2022-08-21 17:20:49 +0430

    add mobile and description for payment and normalization of return messages  (#160)
    * fixed input name mobileNo TO MobileNo for behpardakht driver
    
    * set MobileNo for get user cards and set Description for payment for saddad driver
    
    * Normalization of return messages for sadad driver
    
    Co-authored-by: Mohammad Esmaeilzadeh <md@saprenco.com>

--- src/Drivers/Sadad/Sadad.php
+++ src/Drivers/Sadad/Sadad.php
@@ -68,7 +68,21 @@ class Sadad extends Driver
 
         $signData = $this->encrypt_pkcs7("$terminalId;$orderId;$amount", $key);
         $iranTime = new DateTime('now', new DateTimeZone('Asia/Tehran'));
-        
+
+        //set Description for payment
+        if (!empty($this->invoice->getDetails()['description'])) {
+            $description = $this->invoice->getDetails()['description'];
+        } else {
+            $description = $this->settings->description;
+        }
+
+        //set MobileNo for get user cards
+        if (!empty($this->invoice->getDetails()['mobile'])) {
+            $mobile = $this->invoice->getDetails()['mobile'];
+        } else {
+            $mobile = "";
+        }
+
         $data = array(
             'MerchantId' => $this->settings->merchantId,
             'ReturnUrl' => $this->settings->callbackUrl,
@@ -78,6 +92,8 @@ class Sadad extends Driver
             'TerminalId' => $terminalId,
             'Amount' => $amount,
             'OrderId' => $orderId,
+            'additionalData' => $description,
+            'UserId' => $mobile,
         );
 
         $response = $this
@@ -135,10 +151,9 @@ class Sadad extends Driver
         $key = $this->settings->key;
         $token = $this->invoice->getTransactionId() ?? Request::input('token');
         $resCode = Request::input('ResCode');
-        $message = 'تراکنش نا موفق بود در صورت کسر مبلغ از حساب شما حداکثر پس از 72 ساعت مبلغ به حسابتان برمیگردد.';
 
         if ($resCode != 0) {
-            throw new InvalidPaymentException($message);
+            throw new InvalidPaymentException($this->translateStatus($resCode), $resCode);
         }
 
         $data = array(
@@ -163,8 +178,9 @@ class Sadad extends Driver
 
         $body = json_decode($response->getBody()->getContents());
 
-        if ($body->ResCode != 0) {
-            throw new InvalidPaymentException($message);
+        $bodyResponse = $body->ResCode;
+        if ($bodyResponse != 0) {
+            throw new InvalidPaymentException($this->translateStatus($bodyResponse), $bodyResponse);
         }
 
         /**
@@ -213,7 +229,7 @@ class Sadad extends Driver
 
         return base64_encode($ciphertext);
     }
-    
+
     /**
      * Retrieve payment mode.
      *
@@ -265,5 +281,69 @@ class Sadad extends Driver
         }
 
         return $url;
+    }
+
+    /**
+     * Convert status to a readable message.
+     *
+     * @param $status
+     *
+     * @return mixed|string
+     */
+    private function translateStatus($status)
+    {
+        $translations = [
+            '0' => 'تراکنش با موفقیت انجام شد',
+            '3' => 'پذيرنده کارت فعال نیست لطفا با بخش امور پذيرندگان، تماس حاصل فرمائید',
+            '23' => 'پذيرنده کارت نا معتبر لطفا با بخش امور پذيرندگان، تماس حاصل فرمائید',
+            '58' => 'انجام تراکنش مربوطه توسط پايانه ی انجام دهنده مجاز نمی باشد',
+            '61' => 'مبلغ تراکنش از حد مجاز بالاتر است',
+            '101' => 'مهلت ارسال تراکنش به پايان رسیده است',
+            '1000' => 'ترتیب پارامترهای ارسالی اشتباه می باشد',
+            '1001' => 'پارامترهای پرداخت اشتباه می باشد',
+            '1002' => 'خطا در سیستم- تراکنش ناموفق',
+            '1003' => 'IP پذيرنده اشتباه است',
+            '1004' => 'شماره پذيرنده اشتباه است',
+            '1005' => 'خطای دسترسی:لطفا بعدا تلاش فرمايید',
+            '1006' => 'خطا در سیستم',
+            '1011' => 'درخواست تکراری- شماره سفارش تکراری می باشد',
+            '1012' => 'اطلاعات پذيرنده صحیح نیست، يکی از موارد تاريخ،زمان يا کلید تراکنش اشتباه است',
+            '1015' => 'پاسخ خطای نامشخص از سمت مرکز',
+            '1017' => 'مبلغ درخواستی شما جهت پرداخت از حد مجاز تعريف شده برای اين پذيرنده بیشتر است',
+            '1018' => 'اشکال در تاريخ و زمان سیستم. لطفا تاريخ و زمان سرور خود را با بانک هماهنگ نمايید',
+            '1019' => 'امکان پرداخت از طريق سیستم شتاب برای اين پذيرنده امکان پذير نیست',
+            '1020' => 'پذيرنده غیرفعال شده است',
+            '1023' => 'آدرس بازگشت پذيرنده نامعتبر است',
+            '1024' => 'مهر زمانی پذيرنده نامعتبر است',
+            '1025' => 'امضا تراکنش نامعتبر است',
+            '1026' => 'شماره سفارش تراکنش نامعتبر است',
+            '1027' => 'شماره پذيرنده نامعتبر است',
+            '1028' => 'شماره ترمینال پذيرنده نامعتبر است',
+            '1029' => 'آدرس IP پرداخت در محدوده آدرس های معتبر اعلام شده توسط پذيرنده نیست',
+            '1030' => 'آدرس Domain پرداخت در محدوده آدرس های معتبر اعلام شده توسط پذیرنده نیست',
+            '1031' => 'مهلت زمانی شما جهت پرداخت به پايان رسیده است.لطفا مجددا سعی بفرمایید',
+            '1032' => 'پرداخت با اين کارت , برای پذيرنده مورد نظر شما امکان پذير نیست',
+            '1033' => 'به علت مشکل در سايت پذيرنده, پرداخت برای اين پذيرنده غیرفعال شده است',
+            '1036' => 'اطلاعات اضافی ارسال نشده يا دارای اشکال است',
+            '1037' => 'شماره پذيرنده يا شماره ترمینال پذيرنده صحیح نمیباشد',
+            '1053' => 'خطا: درخواست معتبر، از سمت پذيرنده صورت نگرفته است لطفا اطلاعات پذيرنده خود را چک کنید',
+            '1055' => 'مقدار غیرمجاز در ورود اطلاعات',
+            '1056' => 'سیستم موقتا قطع میباشد.لطفا بعدا تلاش فرمايید',
+            '1058' => 'سرويس پرداخت اينترنتی خارج از سرويس می باشد.لطفا بعدا سعی بفرمایید',
+            '1061' => 'اشکال در تولید کد يکتا. لطفا مرورگر خود را بسته و با اجرای مجدد عملیات پرداخت را انجام دهید',
+            '1064' => 'لطفا مجددا سعی بفرمايید',
+            '1065' => 'ارتباط ناموفق .لطفا چند لحظه ديگر مجددا سعی کنید',
+            '1066' => 'سیستم سرويس دهی پرداخت موقتا غیر فعال شده است',
+            '1068' => 'با عرض پوزش به علت بروزرسانی , سیستم موقتا قطع میباشد',
+            '1072' => 'خطا در پردازش پارامترهای اختیاری پذيرنده',
+            '1101' => 'مبلغ تراکنش نامعتبر است',
+            '1103' => 'توکن ارسالی نامعتبر است',
+            '1104' => 'اطلاعات تسهیم صحیح نیست',
+            '1105' => 'تراکنش بازگشت داده شده است(مهلت زمانی به پايان رسیده است)'
+        ];
+
+        $unknownError = 'خطای ناشناخته رخ داده است. در صورت کسر مبلغ از حساب حداکثر پس از 72 ساعت به حسابتان برمیگردد';
+
+        return array_key_exists($status, $translations) ? $translations[$status] : $unknownError;
     }
 }
