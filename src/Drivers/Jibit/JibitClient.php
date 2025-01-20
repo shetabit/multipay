e402e5f89a880baf42dd7419274de9f9c8812837
bahramsadin <bahramsadin@gmail.com>
2025-01-20 20:21:30 +0330

    Fix phpstan error3: Result of method Shetabit\Multipay\Drivers\Jibit\JibitClient::setAccessToken() (void) is used

--- src/Drivers/Jibit/JibitClient.php
+++ src/Drivers/Jibit/JibitClient.php
@@ -97,7 +97,7 @@ class JibitClient
     private function generateToken(bool $isForce = false)
     {
         if ($isForce === false && $this->cache->has('accessToken')) {
-            return $this->setAccessToken($this->cache->get('accessToken'));
+            $this->setAccessToken($this->cache->get('accessToken'));
         }
         if ($this->cache->has('refreshToken')) {
             $refreshToken = $this->refreshTokens();
