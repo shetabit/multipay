769869aedf04214874c7dd4ca043db3e3ff735f7
Mosi2142 <balbod2142@gmail.com>
2026-08-12 11:19:43 +0330

    Fixed Null safety issue in some rare cases

--- src/Drivers/Zarinpal/Strategies/Sandbox.php
+++ src/Drivers/Zarinpal/Strategies/Sandbox.php
@@ -139,7 +139,7 @@ class Sandbox extends Driver
         $result = json_decode($response->getBody()->getContents(), true);
 
         if (empty($result['data']) || !isset($result['data']['ref_id']) || ($result['data']['code'] != 100 && $result['data']['code'] != 101)) {
-            $bodyResponse = $result['errors']['code'];
+            $bodyResponse = $result['errors']['code'] ?? $result['data']['code'];
             throw new InvalidPaymentException($this->translateStatus($bodyResponse), $bodyResponse);
         }
 
