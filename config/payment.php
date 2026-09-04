d00b95e412c67731b119f8c59a629fc5d46ba090
Azade Ghasemi <46772227+AzadGh95@users.noreply.github.com>
2026-09-04 21:17:05 +0330

    Add Referer header to Zibal requests (#362)
    * Add Referer header to Zibal requests
    
    According to Zibal's latest update, we need to add the `Referer` header to Zibal requests by September 11 (20 Shahrivar).
    https://zib.al/UWHS3PC
    
    * fix(zibal): make referer header configurable
    
    * fix: failing tests

--- config/payment.php
+++ config/payment.php
@@ -357,6 +357,7 @@ return [
             'merchantId' => '',
             'callbackUrl' => 'http://yoursite.com/path/to',
             'description' => 'payment using zibal',
+            'referer' => null,
             'currency' => IranCurrency::TOMAN,
         ],
         'sepordeh' => [
--- src/Drivers/Zibal/Zibal.php
+++ src/Drivers/Zibal/Zibal.php
@@ -87,6 +87,9 @@ class Zibal extends Driver
                     RequestOptions::BODY => json_encode($data),
                     RequestOptions::HEADERS => [
                         'Content-Type' => 'application/json',
+                        ...(($this->settings->referer ?? null) === null ? [] : [
+                            'Referer' => $this->settings->referer,
+                        ]),
                     ],
                     RequestOptions::HTTP_ERRORS => false,
                 ]
@@ -153,6 +156,9 @@ class Zibal extends Driver
                 RequestOptions::HEADERS => [
                     'Accept' => 'application/json',
                     'Content-Type' => 'application/json',
+                    ...(($this->settings->referer ?? null) === null ? [] : [
+                        'Referer' => $this->settings->referer,
+                    ]),
                 ],
                 RequestOptions::HTTP_ERRORS => false,
             ]
